8b9dacae565810c56e1eee5af1417c702d9d05d4
Paola <cfpaola2006@gmail.com>
2025-10-02 19:00:39 +0200

    Made the shapes into an array, added a 'random number generator', created the logic for showing and adding the next piece, added a clear shape function for the 'next piece', added the delay function from lab 1 (currently not using it yet, but could be useful), added an array that saves pieces in the board, so when falling the grid is cleared but it saves the pieces that were already there, rotation still lags, missing movement of pieces + general collision with pieces

--- tetris_pieces.c
+++ tetris_pieces.c
@@ -2,15 +2,17 @@
 This is the main file for the project, where we implement the different parts of the game of tetris, 
 specifically the setup such as the logic and creating and initialising the pieces and/or game.
 */
+#include <stdint.h>
 
 extern void enable_interrupt(void);
+
 #define width_screen 320
 #define height_screen 240
 #define GRID_WIDTH 10     
 #define GRID_HEIGHT 20
 #define block_size 8
 #define GRID_COLOR 0x12 //to see the actual grid
-
+#define MAX_PIECES 300
 
 #define x_offset ((width_screen - GRID_WIDTH * block_size) / 2)
 #define y_offset ((height_screen - (GRID_HEIGHT) * block_size) / 2)
@@ -18,10 +20,40 @@ extern void enable_interrupt(void);
 char grid[GRID_HEIGHT][GRID_WIDTH];  // [row][col]
 int timeoutcount = 0;
 
+//Random number generator
+uint32_t seed;
+
+void set_rand_seed(uint8_t s) {
+  seed = s;
+}
+
+uint8_t rand() {
+  static uint8_t a = 75;
+  static uint8_t c = 179; 
+  static uint8_t modulus = 1 << 6; 
+  seed = (a * seed + c) % modulus;
+  return seed;
+}
+
+//Copied from Lab exercise 1
+void delay( int ms ) {
+    int i;
+    while( ms > 0 ){
+        ms = ms - 1;
+    /* Executing the following for loop should take 1 ms */
+    for( i = 0; i < 4711*ms; i = i + 1 ) /* The constant 4711 must be easy to change! */
+    {
+    /* Do nothing. */
+    }
+    }
+}
+
+
+
+
+
 // VGA framebuffer base address
 volatile char *VGA = (volatile char*) 0x08000000;
-
-
 
 typedef struct {
     char name; //id of the piece
@@ -30,7 +62,57 @@ typedef struct {
     int shape[4][4]; // 4x4 grid representing the shape (1 = block, 0 = empty)
 } shape;
 
- shape current_piece;
+shape current_piece;
+shape next_piece;
+
+shape pieces[] = {
+{0, 0, 0, 0x1F, { //I
+        {1,0,0,0},
+        {1,0,0,0},
+        {1,0,0,0},
+        {1,0,0,0}
+    }
+},
+{1, 0, 0, 0x03, { //J
+    {0,1,0,0},
+    {0,1,0,0},
+    {1,1,0,0},
+    {0,0,0,0}
+}},
+{2, 0, 0, 0xEC, { // L
+    {1,0,0,0},
+    {1,0,0,0},
+    {1,1,0,0},
+    {0,0,0,0}
+}},
+{3, 0, 0, 0xFC, { // O
+        {1,1,0,0},
+        {1,1,0,0},
+        {0,0,0,0},
+        {0,0,0,0}
+}},
+{4, 0, 0, 0x1C, { // S
+        {1,0,0,0},
+        {1,1,0,0},
+        {0,1,0,0},
+        {0,0,0,0}
+}},
+{5, 0, 0, 0x63, { //T
+        {0,1,0,0},
+        {1,1,1,0},
+        {0,0,0,0},
+        {0,0,0,0}
+}},
+{6, 0, 0, 0xE0 , { //Z
+    {1,1,0,0},
+    {0,1,1,0},
+    {0,0,0,0},
+    {0,0,0,0}
+}}
+};
+
+shape locked_pieces[MAX_PIECES];
+int num_locked = 0;
 
 void spawn_piece(shape *current){
     current_piece = *current;
@@ -38,6 +120,11 @@ void spawn_piece(shape *current){
     current_piece.y = 0;
 }
 
+void spawn_next(){
+    current_piece = next_piece;
+    next_piece = pieces[rand() % 7];
+    spawn_piece(&current_piece);
+}
 
 
 void labinit(void)
@@ -50,13 +137,26 @@ void labinit(void)
     volatile unsigned short* tmr_start = (unsigned short*)0x04000024;
     *tmr_start =  0x7;
 
-  
+    volatile int* button_interrupt = (int*)0x040000d8;
+    *button_interrupt = 0x1;
+
+    volatile int* button_edge = (int*)0x040000dc;
+    *button_edge = 0x1;
+
+
+
     enable_interrupt();
 }
 
 int get_btn(void){
   volatile int* btn = (volatile int*)0x040000d0;
   return ((*btn) & 0x1);
+}
+
+int get_sw( void ){
+  volatile int* switches = (volatile int*)0x04000010;
+  return (*switches) & 0x3FF;
+  //printf("%d",i);
 }
 
 void draw_grid(volatile char *VGA){
@@ -85,17 +185,17 @@ void draw_grid(volatile char *VGA){
 }
 
 
-void draw_shape(volatile char *VGA, shape *t) {
+void draw_shape(volatile char *VGA, shape *current) {
     // pointer to VGA memory, pointer to tetris shape, how big
     for (int y = 0; y < 4; y++) {
         for (int x = 0; x < 4; x++) {
             // loops throught 4x4 piece
-            if (t->shape[y][x]) {
+            if (current ->shape[y][x]) {
                     //checks for 0 and 1
 
                 int pixel_index =
-                    (y_offset + (t->y + y) * block_size) * width_screen +
-                    (x_offset + (t->x + x) * block_size);
+                    (y_offset + (current->y + y) * block_size) * width_screen +
+                    (x_offset + (current->x + x) * block_size);
                 //gets top-left corner of piece, shifts to the right 
 
 
@@ -103,7 +203,7 @@ void draw_shape(volatile char *VGA, shape *t) {
                     // counts vertical pixels inside the block
                     for (int col = 0; col < block_size; col++) {
                     // counts horizontal pixels inside the block
-                        VGA[pixel_index + row * width_screen + col] = t->color;
+                        VGA[pixel_index + row * width_screen + col] = current->color;
                     }
                 }
             }
@@ -111,58 +211,45 @@ void draw_shape(volatile char *VGA, shape *t) {
     }
 }
 
-shape I = {0, 0, 0, 0x1F, {
-        {1,0,0,0},
-        {1,0,0,0},
-        {1,0,0,0},
-        {1,0,0,0}
+void draw_next_piece(volatile char *VGA, shape *next){ // draws the future piece inside a new grid that shows this.
+    int preview_x = width_screen - 60;
+    int preview_y = 40;
+
+    for (int y= 0; y < 4; y++){
+        for (int x =0; x<4; x++){
+            if (next -> shape[y][x]){
+                int pixel_index =
+                    (preview_y + y*block_size) * width_screen + (preview_x + x*block_size);
+                
+                    for (int row = 0; row < block_size; row++){
+                        for (int col = 0; col < block_size; col++){
+                            VGA[pixel_index + row*width_screen + col] = next->color;
+                        }
+                    }
+            }
+        }
     }
-    };    
-    
-shape J = {1, 0, 0, 0x03, {
-    {0,1,0,0},
-    {0,1,0,0},
-    {1,1,0,0},
-    {0,0,0,0}
-}};
+}
 
-shape L = {2, 0, 0, 0xEC, {
-    {1,0,0,0},
-    {1,0,0,0},
-    {1,1,0,0},
-    {0,0,0,0}
-}};
+void clear_shape(volatile char *VGA, shape *next){
+    int preview_x = width_screen - 60;
+    int preview_y = 40;
 
-shape O = {3, 0, 0, 0xFC, {
-        {1,1,0,0},
-        {1,1,0,0},
-        {0,0,0,0},
-        {0,0,0,0}
+    for (int y = 0; y < 4; y++){
+        for (int x = 0; x < 4; x++){
+            if (next->shape[y][x]){
+                int pixel_index =
+                    (preview_y + y*block_size) * width_screen + (preview_x + x*block_size);
+
+                for (int row = 0; row < block_size; row++){
+                    for (int col = 0; col < block_size; col++){
+                        VGA[pixel_index + row*width_screen + col] = 0; // clear to black
+                    }
+                }
+            }
+        }
     }
-    };
-
-
-shape S = {4, 0, 0, 0x1C, {
-        {1,0,0,0},
-        {1,1,0,0},
-        {0,1,0,0},
-        {0,0,0,0}
-    }};
-
-shape T = {5, 0, 0, 0x63, {
-        {0,1,0,0},
-        {1,1,1,0},
-        {0,0,0,0},
-        {0,0,0,0}
-    }};
-
-
-shape Z = {6, 0, 0, 0xE0 , {
-    {1,1,0,0},
-    {0,1,1,0},
-    {0,0,0,0},
-    {0,0,0,0}
-}};
+}
 
 void rotate_piece(shape *p) {
     int temp[4][4];
@@ -179,36 +266,53 @@ void rotate_piece(shape *p) {
             p->shape[y][x] = temp[y][x];
 }
 
-int last_btn_state = 0;
+
+
+
+
+
+
 
 void tick(volatile char *VGA){
-    draw_grid(VGA); // redraw background
+    
+    volatile int button_pressed = get_btn();
 
-
-    int btn = get_btn();
-
-    if (btn && !last_btn_state){
+    if (button_pressed){
+        
         rotate_piece(&current_piece);
     }
 
-    last_btn_state = btn;
     // compute piece height
     int max_row = 0;
-    for (int y = 0; y < 4; y++)
-        for (int x = 0; x < 4; x++)
-            if (current_piece.shape[y][x] && y + 1 > max_row)
+    for (int y = 0; y < 4; y++){
+        for (int x = 0; x < 4; x++){
+            if (current_piece.shape[y][x] && y + 1 > max_row){
                 max_row = y + 1;  // max_row = height of the piece
-
-    // Only fall if bottom of piece is below the last row
-    if (current_piece.y + max_row < GRID_HEIGHT)
+    }}}
+    // falls unless bottom of piece is below the last row
+    if (!(current_piece.y + max_row < GRID_HEIGHT)){
+        if (num_locked < MAX_PIECES){
+            locked_pieces[num_locked++] = current_piece;
+        }
+        clear_shape(VGA, &next_piece);
+        spawn_next();
+        draw_next_piece(VGA, &next_piece);
+        
+    }
+    else{
         current_piece.y += 1;  // piece falls
-
-    draw_shape(VGA, &current_piece); // draw at new position
+        
+    }
+    draw_grid(VGA); // redraw background
+    for (int i = 0; i < num_locked; i++){
+        draw_shape(VGA, &locked_pieces[i]);
+    }
+    draw_shape(VGA, &current_piece);
+    
 }
-
 void handle_interrupt(unsigned cause) 
 {
-    if (cause == 16){
+    if (cause == 16){ //Timer
         volatile unsigned short* tmr_stat = (unsigned short*)0x04000020;
         *tmr_stat = 0x0;
 
@@ -218,22 +322,29 @@ void handle_interrupt(unsigned cause)
             tick(VGA);
         }
     }
+    
 }
+
+
 
 int main() {
     labinit();
+
+    int sw = get_sw();
+    set_rand_seed(sw); //randomise the piece that will follow through.
+
+
+    next_piece = pieces[rand()%7];
     
-    spawn_piece(&L); //test
+    spawn_next();
+    
+   
     
     draw_grid(VGA);
-    
     draw_shape(VGA, &current_piece);
-    //draw_shape(VGA, &O);
-    //draw_shape(VGA, &I);
-    //draw_shape(VGA, &S);
-    //draw_shape(VGA, &Z);
-    //draw_shape(VGA, &L);
-    //draw_shape(VGA, &J);
+    draw_next_piece(VGA, &next_piece);
+    
+   
 
     
     while(1) {
